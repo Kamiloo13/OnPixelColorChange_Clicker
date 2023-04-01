7455d5356d9b0eb4d2725cc99efc9777837839c6
Kamil Hibszer <79232750+Kamiloo13@users.noreply.github.com>
2023-04-01 19:38:19 +0200

    Dodaj pliki projektów.

---
+++ OnPixelColorChange_Clicker/DeviceManipulator.cs
@@ -0,0 +1,170 @@
+﻿using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace OnPixelColorChange_Clicker
+{
+    /// <summary>
+    /// Provides methods for simulating mouse and keyboard actions using Windows API.
+    /// </summary>
+    internal class DeviceManipulator
+    {
+        private const uint MouseEventRightDown = 0x0008;
+        private const uint MouseEventRightUp = 0x0010;
+        private const uint KeyEventKeyDown = 0x0000;
+        private const uint KeyEventKeyUp = 0x0002;
+        private const ushort KeyCodeD1 = 0x31;
+        private const ushort KeyCodeD2 = 0x32;
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern uint SendInput(uint nInputs, INPUT[] pInputs, int cbSize);
+
+        [DllImport("user32.dll")]
+        public static extern short GetAsyncKeyState(int vKey);
+
+        [DllImport("user32.dll")]
+        public static extern bool GetCursorPos(out Point lpPoint);
+
+        private enum EventType
+        {
+            MouseEvent = 0,
+            KeyEvent = 1,
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct INPUT
+        {
+            public int Type;
+            public INPUTUNION Union;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        private struct INPUTUNION
+        {
+            [FieldOffset(0)]
+            public MOUSEINPUT MouseInput;
+            [FieldOffset(0)]
+            public KEYBDINPUT KeyboardInput;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct MOUSEINPUT
+        {
+            public int X;
+            public int Y;
+            public uint MouseData;
+            public uint Flags;
+            public uint Time;
+            public IntPtr ExtraInfo;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct KEYBDINPUT
+        {
+            public ushort VirtualKey;
+            public ushort ScanCode;
+            public uint Flags;
+            public uint Time;
+            public IntPtr ExtraInfo;
+        }
+
+        private static readonly INPUT mouseRightDownInput = new()
+        {
+            Type = (int)EventType.MouseEvent,
+            Union = new INPUTUNION { MouseInput = new MOUSEINPUT { Flags = MouseEventRightDown } }
+        };
+
+        private static readonly INPUT mouseRightUpInput = new()
+        {
+            Type = (int)EventType.MouseEvent,
+            Union = new INPUTUNION { MouseInput = new MOUSEINPUT { Flags = MouseEventRightUp } }
+        };
+
+        private static readonly INPUT keyboardD1KeyDownInput = new()
+        {
+            Type = (int)EventType.KeyEvent,
+            Union = new INPUTUNION
+            {
+                KeyboardInput = new KEYBDINPUT
+                {
+                    VirtualKey = KeyCodeD1,
+                    Flags = KeyEventKeyDown
+                }
+            }
+        };
+
+        private static readonly INPUT keyboardD1KeyUpInput = new()
+        {
+            Type = (int)EventType.KeyEvent,
+            Union = new INPUTUNION
+            {
+                KeyboardInput = new KEYBDINPUT
+                {
+                    VirtualKey = KeyCodeD1,
+                    Flags = KeyEventKeyUp
+                }
+            }
+        };
+
+        private static readonly INPUT keyboardD2KeyDownInput = new()
+        {
+            Type = (int)EventType.KeyEvent,
+            Union = new INPUTUNION
+            {
+                KeyboardInput = new KEYBDINPUT
+                {
+                    VirtualKey = KeyCodeD2,
+                    Flags = KeyEventKeyDown
+                }
+            }
+        };
+
+        private static readonly INPUT keyboardD2KeyUpInput = new()
+        {
+            Type = (int)EventType.KeyEvent,
+            Union = new INPUTUNION
+            {
+                KeyboardInput = new KEYBDINPUT
+                {
+                    VirtualKey = KeyCodeD2,
+                    Flags = KeyEventKeyUp
+                }
+            }
+        };
+
+        public static void SimulateMouseActions()
+        {
+            var inputs = new[] { mouseRightDownInput, mouseRightUpInput };
+
+            // Simulate a right mouse button click
+            _ = SendInput((uint)inputs.Length, inputs, Marshal.SizeOf(typeof(INPUT)));
+
+            Thread.Sleep(800);
+
+            inputs = new[] { keyboardD2KeyDownInput, keyboardD2KeyUpInput };
+
+            // Simulate a keyboard D1 key press
+            _ = SendInput((uint)inputs.Length, inputs, Marshal.SizeOf(typeof(INPUT)));
+
+            Thread.Sleep(100);
+
+            inputs = new[] { keyboardD1KeyDownInput, keyboardD1KeyUpInput };
+
+            // Simulate a keyboard D2 key press
+            _ = SendInput((uint)inputs.Length, inputs, Marshal.SizeOf(typeof(INPUT)));
+
+            Thread.Sleep(100);
+
+            inputs = new[] { mouseRightDownInput };
+
+            // Simulate a right mouse button click
+            _ = SendInput((uint)inputs.Length, inputs, Marshal.SizeOf(typeof(INPUT)));
+
+            Thread.Sleep(500);
+
+            inputs = new[] { mouseRightUpInput };
+
+            // Simulate a right mouse button click
+            _ = SendInput((uint)inputs.Length, inputs, Marshal.SizeOf(typeof(INPUT)));
+        }
+    }
+}
---
+++ OnPixelColorChange_Clicker/Program.cs
@@ -0,0 +1,87 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace OnPixelColorChange_Clicker
+{
+    class Program
+    {
+        private Point pixelLocation;
+        private Color pixelColor;
+        private readonly Bitmap bitmap = new(1, 1, PixelFormat.Format32bppArgb);
+        private bool isClicking;
+        private const int KeyboardEventNumpad1 = 0x61;
+        private const int KeyboardEventNumpad2 = 0x62;
+        private const int KeyboardEventNumpad3 = 0x63;
+        private const int WaitLoopDelay = 125;
+        private const int WaitLoopIterations = 200;
+        private const int WaitDelayAfterClick = 2000;
+
+        private void InitializeApp()
+        {
+            Console.Title = "OnPixelColorChange Clicker by Kamil Hibszer";
+            Console.WriteLine("======================================================");
+            Console.WriteLine("[ HOTKEYS ]\n");
+            Console.WriteLine("NumPad1 = save pixel position");
+            Console.WriteLine("NumPad2 = START");
+            Console.WriteLine("NumPad3 = STOP\n\n");
+            Console.WriteLine("======================================================");
+            Console.WriteLine("\n\n[ LOG ]\n");
+
+            while (true)
+            {
+                if (DeviceManipulator.GetAsyncKeyState(KeyboardEventNumpad1) != 0)
+                {
+                    DeviceManipulator.GetCursorPos(out pixelLocation);
+                    pixelColor = GetPixelColor(pixelLocation);
+                    Console.WriteLine("Mouse X = " + pixelLocation.X);
+                    Console.WriteLine("Mouse Y = " + pixelLocation.Y);
+                    Thread.Sleep(100);
+                }
+
+                if (DeviceManipulator.GetAsyncKeyState(KeyboardEventNumpad2) != 0)
+                {
+                    Console.WriteLine("Activated !");
+                    Thread.Sleep(100);
+                    isClicking = true;
+
+                    while (isClicking)
+                    {
+                        var newPixelColor = GetPixelColor(pixelLocation);
+                        if (newPixelColor == pixelColor)
+                        {
+                            DeviceManipulator.SimulateMouseActions();
+                            Thread.Sleep(WaitDelayAfterClick);
+                        }
+
+                        var waitLoopIterations = WaitLoopIterations;
+                        while (waitLoopIterations > 0)
+                        {
+                            Thread.Sleep(WaitLoopDelay);
+                            waitLoopIterations -= WaitLoopDelay;
+                            if (DeviceManipulator.GetAsyncKeyState(KeyboardEventNumpad3) != 0)
+                            {
+                                isClicking = false;
+                                Console.WriteLine("Stopping the clicker..");
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private Color GetPixelColor(Point point)
+        {
+            using (var graphics = Graphics.FromImage(bitmap))
+            {
+                graphics.CopyFromScreen(point, new Point(0, 0), new Size(1, 1));
+            }
+            return bitmap.GetPixel(0, 0);
+        }
+
+        public static void Main()
+        {
+            new Program().InitializeApp();
+        }
+    }
+}
